d4c45110f29eb726e5a3e6574d485d584ddf4e89
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-10-07 10:35:55 -0400

    messing aroudn with reading input

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+
+	"os"
 
 	"github.com/fatih/color"
 )
 
 func main() {
-	fmt.Println("Hello World1")
-	color.Red("test")
-	fmt.Println("test2")
-	string1 := "test3" + " " + "lol" + "1 + 1"
-	color.Green(string1)
+	fmt.Print("Enter some text : ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	color.Green("You entered, %s", input)
+	color.Blue("Is that right?")
+	fmt.Print("> ")
+	input, _ = reader.ReadString('\n')
+	if input == "yes\n" {
+		color.Green("Good!")
+	} else if input == "no\n" {
+		color.Red("Liar!")
+	} else {
+		color.Yellow("I did not understand")
+	}
 }
