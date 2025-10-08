28eb7c44c286fcb092c1fb7b1fc963634fc940ac
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-10-08 18:21:54 -0400

    still messing around with strings.. started a plan of the game's map

--- main.go
+++ main.go
@@ -7,19 +7,23 @@ import (
 	"os"
 
 	"github.com/fatih/color"
+
+	"strings"
 )
 
 func main() {
 	fmt.Print("Enter some text : ")
 	reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
+	input = strings.TrimSpace(input)
 	color.Green("You entered, %s", input)
 	color.Blue("Is that right?")
 	fmt.Print("> ")
 	input, _ = reader.ReadString('\n')
-	if input == "yes\n" {
+	input = strings.TrimSpace(input)
+	if input == "yes" {
 		color.Green("Good!")
-	} else if input == "no\n" {
+	} else if input == "no" {
 		color.Red("Liar!")
 	} else {
 		color.Yellow("I did not understand")
