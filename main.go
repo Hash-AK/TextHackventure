cc5c231c8d2534131ccc6b596ae690e9b1585e11
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-10-09 19:40:29 -0400

    added feature element, note, help command menu

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ type Room struct {
 	Name        string
 	Description string
 	Exits       map[string]*Exit
+	Features    map[string]string
 }
 
 type Player struct {
@@ -32,8 +33,9 @@ func main() {
 	}
 	hut := Room{
 		Name:        "The Abandonned Hut",
-		Description: "You stumble upon a vast clearing. In it's center, an old, visibly abandonned hut stands. It's thathched roof is smashed on quite a few place.",
+		Description: "You stumble upon a vast clearing. In it's center, an old, visibly abandonned hut stands. It's thathched roof is smashed on quite a few place. As you enter it, you see and old, dusty table. A small note is placed on it",
 		Exits:       make(map[string]*Exit),
+		Features:    make(map[string]string),
 	}
 	beginningRoom.Exits["north"] = &Exit{
 		Destination: &hut,
@@ -43,6 +45,7 @@ func main() {
 		Destination: &beginningRoom,
 		Description: "You head back into the oppressive darkness of the forest",
 	}
+	hut.Features["note"] = "The note reads: \nThe beast of fire, \nGuardian of The Gate \nFears only the fire \nOf the earthsoul crystals..."
 	player := Player{CurrentRoom: &beginningRoom}
 	reader := bufio.NewReader(os.Stdin)
 	color.Set(color.FgGreen)
@@ -51,7 +54,7 @@ func main() {
 	fmt.Println("TextHackventure!")
 	color.Unset()
 	fmt.Println("")
-	fmt.Println("		////////////////		")
+	fmt.Println("***************************")
 	fmt.Println("")
 	fmt.Println("Chapter 1 : " + beginningRoom.Name)
 	fmt.Println(beginningRoom.Description)
@@ -75,13 +78,14 @@ func main() {
 			fmt.Println("help - display this help menu")
 			fmt.Println("describe - describe the place you are currently in")
 			fmt.Println("go [north/south/east/west] - go in the choosed direction")
+			fmt.Println("read [note/sign] - read an element of the place")
 		case "quit":
 			fmt.Println("Goodbye, traveller...")
 			os.Exit(0)
 		case "go":
 			if exit, ok := player.CurrentRoom.Exits[argument]; ok {
 				fmt.Println(exit.Description)
-				fmt.Println("////////////")
+				fmt.Println("***************************")
 				player.CurrentRoom = exit.Destination
 				fmt.Println(player.CurrentRoom.Description)
 			} else {
@@ -90,6 +94,12 @@ func main() {
 		case "describe":
 			fmt.Println("You are in : " + player.CurrentRoom.Name)
 			fmt.Println(player.CurrentRoom.Description)
+		case "read":
+			if text, ok := player.CurrentRoom.Features[argument]; ok {
+				fmt.Println(text)
+			} else {
+				fmt.Println("You cannot read this")
+			}
 
 		default:
 			fmt.Println(cleanInput + " : command not found. Type 'help' to get a list of the commands.")
