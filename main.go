468bf7e35c8d7f68ce40af1bee9694b259218356
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-10-09 15:22:14 -0400

    added command reading loop;

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/fatih/color"
 )
@@ -13,9 +16,10 @@ type Room struct {
 
 func main() {
 	beginningRoom := Room{
-		Name:        "Dark Forest",
-		Description: "You in a deep, dark, mysterious forest.",
+		Name:        "The Dark Forest",
+		Description: "You are in a deep, dark, mysterious forest. Tall, majestuous trees surround you from all sides, except for a tiny path heading north, deeper and deeper into the woods. The air is cool and damp.",
 	}
+	reader := bufio.NewReader(os.Stdin)
 	color.Set(color.FgGreen)
 	fmt.Print("Welcome to ")
 	color.Set(color.FgBlue)
@@ -24,7 +28,18 @@ func main() {
 	fmt.Println("")
 	fmt.Println("		////////////////		")
 	fmt.Println("")
-	fmt.Println("Chapter 1 : The " + beginningRoom.Name)
+	fmt.Println("Chapter 1 : " + beginningRoom.Name)
 	fmt.Println(beginningRoom.Description)
+	for {
+		fmt.Print("> ")
+		input, _ := reader.ReadString('\n')
+		command := strings.TrimSpace(input)
+		fmt.Printf("You entered the command: %s\n", command)
+		switch command {
+		case "quit":
+			fmt.Println("Goodbye, traveller...")
+			os.Exit(0)
+		}
+	}
 
 }
