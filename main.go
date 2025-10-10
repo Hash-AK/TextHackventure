5bb0160a5140e356b771d1b2fafcdcaf9df82c71
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2025-10-10 11:45:45 -0400

    added crystalCave room

--- main.go
+++ main.go
@@ -47,8 +47,15 @@ func main() {
 	}
 	caveEntrance := Room{
 		Name:        "The Cave Entrance",
-		Description: "After entering the cave entrance, you feel the heavy weight of tons and tons of rock surroudning you. Suddenly, you reach the end of the tunnel. The ",
+		Description: "After entering the cave entrance, you feel the heavy weight of tons and tons of rock surroudning you. Suddenly, you reach the end of the tunnel. Everything beyond is obstructed by big boulders. To continue, you will need to clean up the path...",
+		Exits:       make(map[string]*Exit),
 	}
+
+	crystalCave := Room{
+		Name:        "The Crystal Cave",
+		Description: "As you look around, you realize that weird little crystals emerge of the rock. They cover the walls and ceiling of the cave, makink long, brilliant veins. A long-forgotten sign lies on one wall. You realize that the little lamps you saw earlier aren't usual lamps : they glow from a weird, magical green flame...",
+	}
+
 	beginningRoom.Exits["north"] = &Exit{
 		Destination: &hut,
 		Description: "As you walk carefully along the small path, the trees slowly close behind you, hiidng any trace of passage. You walk there a long, long time, so long you even forget the perception of time. Finally, you see light in front of you",
@@ -59,17 +66,26 @@ func main() {
 	}
 	hut.Exits["east"] = &Exit{
 		Destination: &caveEntrance,
-		Description: "You head on the small path going east. The forest lighter, and you see big boulders on the side of the path. Soon enough you reach the foot of the mountain. A mysterious cave entrance is there, just in front of you",
+		Description: "You head on the small path going east. The forest become lighter, and you see big boulders on the side of the path. Soon enough you reach the foot of the mountain. A mysterious cave entrance is there, just in front of you.",
 	}
-	hut.Features["note"] = "The note reads: \nThe beast of fire, \nGuardian of The Gate \nFears only the fire \nOf the earthsoul crystals..."
+	hut.Features["note"] = "The note reads: \nThe beast of fire, \nGuardian of The Gate \nFears only the green sparks \nOf the earthsoul crystals..."
 	hut.Items["pickaxe"] = &Item{
 		Name:        "pickaxe",
 		Description: "A sturdy-looking pickaxe. Perfect to break big rocks.",
+	}
+	caveEntrance.Exits["west"] = &Exit{
+		Destination: &hut,
+		Description: "You head back to the east, toward the clearing and the abandonned hut you saw earlier.",
+	}
+	crystalCave.Exits["west"] = &Exit{
+		Destination: &caveEntrance,
+		Description: "You head back toward the cave entrance, in the tight tunnel. You're temporarly blinded due to the change of the ligh, but then you can continue forward. Soon enough you see the light of the day",
 	}
 	player := Player{
 		CurrentRoom: &beginningRoom,
 		Inventory:   make(map[string]*Item),
 	}
+
 	reader := bufio.NewReader(os.Stdin)
 	color.Set(color.FgGreen)
 	fmt.Print("Welcome to ")
@@ -102,6 +118,8 @@ func main() {
 			fmt.Println("describe - describe the place you are currently in")
 			fmt.Println("go [north/south/east/west] - go in the choosed direction")
 			fmt.Println("read [note/sign] - read an element of the place")
+			fmt.Println("take [item] - take an item by it's name.")
+			fmt.Println("inventory - list the inventory you have.")
 		case "quit":
 			fmt.Println("Goodbye, traveller...")
 			os.Exit(0)
@@ -141,6 +159,28 @@ func main() {
 					fmt.Println(" * ", item.Name, " - ", item.Description)
 				}
 			}
+		case "use":
+			if argument == "pickaxe" {
+
+				if _, hasPickaxe := player.Inventory["pickaxe"]; hasPickaxe {
+
+					if player.CurrentRoom == &caveEntrance {
+						fmt.Println("You swing the pickaxe and shatter the boulders! Slowly, you manage to create a passage big enough to let you pass. The tunnel continue toward the east, even further into the mountain...")
+						caveEntrance.Exits["east"] = &Exit{
+							Destination: &crystalCave,
+							Description: "As you walk trought this thight tunnel, you hear the rhytmed sound of water drop falling from the ceiling. Since how long did no one came here? Where does the tunnels go? After a few minutes, you see a weird light. Suddenly, the tunnel ends in an enormous cave, lighten up by mysterious little lamps...",
+						}
+					} else {
+						fmt.Println("You cannot use the pickaxe here!")
+					}
+
+				} else {
+					fmt.Println("You don't have a pickaxe")
+				}
+			} else {
+				fmt.Println("You can't use that.")
+			}
+
 		default:
 			fmt.Println(cleanInput + " : command not found. Type 'help' to get a list of the commands.")
 		}
